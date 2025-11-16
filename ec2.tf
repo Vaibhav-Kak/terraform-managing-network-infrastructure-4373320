a2809bac95189366094e9e87d91345d9274cb4f5
Vaibhav Kak <vaibhavkak.prof.ac@gmail.com>
2025-11-16 16:40:59 +0530

    modified cidr block for ingress to allow connecting to public subnet ec2 instance using ssh

--- ec2.tf
+++ ec2.tf
@@ -15,38 +15,38 @@ resource "aws_instance" "dev" {
   subnet_id                   = aws_subnet.dev[count.index].id
   associate_public_ip_address = true
 
-  tags    = {
-    Name  = var.instances[count.index]
+  tags = {
+    Name = var.instances[count.index]
   }
 
 }
 
 resource "aws_security_group" "public" {
-  vpc_id      = aws_vpc.dev.id
-  name        = var.public_sg
+  vpc_id = aws_vpc.dev.id
+  name   = var.public_sg
 
   ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = [var.public_ip]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
-  tags    = {
-    Name  = var.public_sg
+  tags = {
+    Name = var.public_sg
   }
 
 }
 
 resource "aws_network_interface_sg_attachment" "public" {
-  security_group_id     = aws_security_group.public.id
-  network_interface_id  = aws_instance.dev[0].primary_network_interface_id
+  security_group_id    = aws_security_group.public.id
+  network_interface_id = aws_instance.dev[0].primary_network_interface_id
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,13 +1,13 @@
 # network values
-vpc_name       = "vpc-dev"
-vpc_cidr       = "10.5.0.0/20"
-subnets        = ["sub-public", "sub-private"]
-igw            = "igw-dev"
-public_rtb     = "rtb-public"
-public_sg      = "grp-public"
+vpc_name   = "vpc-dev"
+vpc_cidr   = "10.5.0.0/20"
+subnets    = ["sub-public", "sub-private"]
+igw        = "igw-dev"
+public_rtb = "rtb-public"
+public_sg  = "grp-public"
 
 # instance values
-instance_type  = "t3.nano"
-ami_id         = "ami-0f0ba639982a32edb"
-instances      = ["ec2-public", "ec2-private"]
-public_key     = "keys/ec2.pub"
+instance_type = "t3.nano"
+ami_id        = "ami-0f0ba639982a32edb"
+instances     = ["ec2-public", "ec2-private"]
+public_key    = "keys/ec2.pub"
